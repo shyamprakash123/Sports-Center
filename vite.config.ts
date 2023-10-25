1dc61546a61e014873e746e5881f76c236ce8731
Shyam prakash <shyamprakash9959@gmail.com>
2023-10-25 18:07:50 +0530

    Resolved pwa image error.

--- vite.config.ts
+++ vite.config.ts
@@ -16,27 +16,27 @@ export default defineConfig({
         short_name: "Sport Center",
         icons: [
           {
-            src: "./src/assets/images/favicon.ico",
+            src: "/favicon.ico",
             sizes: "64x64 32x32 24x24 16x16",
             type: "image/x-icon",
           },
           {
-            src: "./src/assets/images/favicon-16x16.png",
+            src: "/favicon-16x16.png",
             type: "image/png",
             sizes: "16x16",
           },
           {
-            src: "./src/assets/images/favicon-32x32.png",
+            src: "/favicon-32x32.png",
             type: "image/png",
             sizes: "32x32",
           },
           {
-            src: "./src/assets/images/android-launchericon-192-192.png",
+            src: "/android-launchericon-192-192.png",
             type: "image/png",
             sizes: "192x192",
           },
           {
-            src: "./src/assets/images/android-launchericon-512-512.png",
+            src: "/android-launchericon-512-512.png",
             type: "image/png",
             sizes: "512x512",
             purpose: "any maskable", // Icon format that ensures that your PWA icon looks great on all Android devices
